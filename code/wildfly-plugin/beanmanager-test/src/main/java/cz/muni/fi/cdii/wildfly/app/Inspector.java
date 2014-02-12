ea178d1f83a852dc010db60160c96dc33f2a4d2a
jakub-n <czerny.jakub@gmail.com>
2014-02-12 13:23:58 +0100

    beanmanager-test - rest added

--- code/wildfly-plugin/beanmanager-test/src/main/java/cz/muni/fi/cdii/wildfly/app/Inspector.java
+++ code/wildfly-plugin/beanmanager-test/src/main/java/cz/muni/fi/cdii/wildfly/app/Inspector.java
@@ -15,20 +15,7 @@ public class Inspector {
 	@Inject
 	private BeanManager beanManager;
 	
-//	@Resource(mappedName="java:comp/BeanManager")
-//	private BeanManager beanManager2;
-	
 	public Set<Bean<?>> getBeans() {
-//		BeanManager beanManager3;
-//		try {
-//			beanManager3 = (BeanManager) InitialContext.doLookup("java:comp/BeanManager");
-//		} catch (NamingException ex) {
-//			throw new RuntimeException(ex);
-//		}
-//
-//		System.out.println("beanmanager: " + beanManager);
-//		System.out.println("beanmanager2: " + beanManager2);
-//		System.out.println("beanmanager3: " + beanManager3);
 		Set<Bean<?>> beans = this.beanManager.getBeans(Object.class,new AnnotationLiteral<Any>() {});
 		return beans;
 	}
