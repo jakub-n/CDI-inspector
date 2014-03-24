9bec525d58571e3fd73ae9a0d7a5f8b55a155b68
jakub-n <czerny.jakub@gmail.com>
2014-03-24 01:28:01 +0100

    trivia

--- code/wildfly-plugin/cdii-extension/deploy.sh
+++ code/wildfly-plugin/cdii-extension/deploy.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-mkdir -p /home/watt/opt/wildfly/wildfly-8.0.0.Final/modules/cz/muni/fi
-cp -r $(dirname ${BASH_SOURCE[0]})/target/module/cz/muni/fi/cdii \
-  /home/watt/opt/wildfly/wildfly-8.0.0.Final/modules/cz/muni/fi/
+MODULE_DIR=/home/watt/opt/wildfly/wildfly-8.0.0.Final/modules/cz/muni/fi/cdii
+mkdir -p ${MODULE_DIR}
+rm -rf ${MODULE_DIR}/main
+cp -r $(dirname ${BASH_SOURCE[0]})/target/module/cz/muni/fi/cdii/main \
+  ${MODULE_DIR}
